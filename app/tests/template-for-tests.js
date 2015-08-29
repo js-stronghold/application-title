ae4a87e28e59a05d295f2f971a5f1dd21583e70f
Peter Velkov <kidroca@gmail.com>
2015-08-29 14:48:08 +0300

    Updated Template for tests

--- app/tests/template-for-tests.js
+++ app/tests/template-for-tests.js
@@ -1,6 +1,21 @@
+var requirejs = require('requirejs');
 var expect = require('chai').expect;
 
+requirejs.config({
+	baseUrl: '',
+	nodeRequire: 'require'
+});
+
 describe('Template', function() {
+
+	// var yourModule;
+	// before(function(done) {
+	// 	requirejs(['scripts/content'], function(module) {
+	// 		yourModule = module;
+	// 		done();
+	// 	});
+	// });
+
 	var number;
 	beforeEach(function(done) {
 		number = 2;
