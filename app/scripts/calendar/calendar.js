24feca943470949476732e3a85d463da606f2d32
StanimiraHK <stanimira.kormeva.4@gmail.com>
2015-09-01 18:35:03 +0300

    Fixed calendar.js - work with template

--- app/scripts/calendar/calendar.js
+++ app/scripts/calendar/calendar.js
@@ -1,33 +1,24 @@
-define('calendar', ['jquery','underscore','calendar/database', 'extensions/date'],
-	function($, _, database) {
+define('calendar', ['libs/jquery','libs/underscore','libs/handlebars''calendar/database', 'extensions/date'],
+	function($, _, Handlebars, database) {
 		 $.fn.calendar = function () {
         var WEEK_DAY_NAMES = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
         var $this = this;
         var currentDate = new Date();
-        var wrapper = $this.parent();
-        var picker = $this.addClass('datepicker').wrap(wrapper);
 
-        var controls = $('<div />').addClass('controls').appendTo(picker);
-
-        var leftBtn = $('<button />').addClass('btn btn-previous').text('<').attr('data-operation', '-1').appendTo(controls);
-        var innerMonth = $('<div />').addClass('current-month').text(currentDate.getMonthName() + ' ' + currentDate.getFullYear()).appendTo(controls);
-        var rightBtn = $('<button />').addClass('btn btn-next').text('>').attr('data-operation', '1').appendTo(controls);
-
-        var calendar = buildCalendar().appendTo(picker);
+        var sorce = $('#calendar-template').html();
+        var template = Handlebars.compile(sorce);
+        var calendar = buildCalendar();
+        var result = template(calendar);
 
         highlightDaysWithContent(currentDate);
 
         function buildCalendar(date) {
             var CALENDAR_ROWS = 6;
             date = date || new Date(); // this is correct
-            var year = year || date.getFullYear(); // ?? this will always be date.getFullYear() because it starts with "var year"
-            var month = month || date.getMonth(); // ?? same
-            var calendar = $('<table />').addClass('calendar');
-            var headerRow = $('<tr />').appendTo(calendar);
-            for (var i = 0, len = WEEK_DAY_NAMES.length; i < len; i++) {
-                $('<th />').text(WEEK_DAY_NAMES[i]).appendTo(headerRow);
-            }
+            var year = date.getFullYear(); 
+            var month = date.getMonth(); 
+            var rows = [];
 
             var firstDayOfMonth = new Date(year, month, 1);
             var firstDayOfMonthWeekDay = firstDayOfMonth.getDay();
@@ -39,7 +30,7 @@ define('calendar', ['jquery','underscore','calendar/database', 'extensions/date'
             var currentDayRendered = 1;
 
             for (var i = 0; i < CALENDAR_ROWS; i += 1) {
-                var row = $('<tr />').appendTo(calendar);
+                var row = [];
                 // previous month numbers
                 if (!previousMonthRendered) {
                     var previousMonthDays = firstDayOfMonthWeekDay;
@@ -51,7 +42,7 @@ define('calendar', ['jquery','underscore','calendar/database', 'extensions/date'
                     }
 
                     for (var j = startIndex, len = previousMonthLastDay; j <= len; j += 1) {
-                        $('<td />').addClass('another-month').text(j).appendTo(row);
+                        row.push({date: ''});
                         startNext += 1;
                     }
                     previousMonthRendered = true;
@@ -59,13 +50,12 @@ define('calendar', ['jquery','underscore','calendar/database', 'extensions/date'
 
                 // current month numbers
                 for (var j = startNext; j < 7; j += 1) {
-                    var cell = $('<td />').text(currentDayRendered).appendTo(row);
                     if (!inNextMonth) {
-                        cell.addClass('current-month');
+                        row.push({date: currentDayRendered});
                     }
                     else {
                         // next month numbers
-                        cell.addClass('another-month');
+                        row.push({date: ''});
                     }
 
                     currentDayRendered += 1;
@@ -76,17 +66,23 @@ define('calendar', ['jquery','underscore','calendar/database', 'extensions/date'
 
                     startNext += 1;
                 }
-
+                rows.push(row);
                 startNext = 0;
             }
 
-            return calendar;
+
+            return {
+                month: month;
+                headers: WEEK_DAY_NAMES;
+                calendarRows: rows;
+
+            };
         }
 
         controls.on('click', 'button', function () {
             var operation = parseInt($(this).data('operation'));
             var date = new Date(currentDate.setMonth(currentDate.getMonth() + operation));
-            buildCalendar(date).replaceAll('.calendar');
+            buildCalendar(date);
             highlightDaysWithContent(date);
             setInnerMonth(date);
         });
@@ -115,6 +111,5 @@ define('calendar', ['jquery','underscore','calendar/database', 'extensions/date'
         }
 
         // append control to DOM
-        wrapper.append(picker);
     }
 });
