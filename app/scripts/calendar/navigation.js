b956193ff91238d4efad38fbfc195c275ad96b86
StanimiraHK <stanimira.kormeva.4@gmail.com>
2015-09-03 13:44:44 +0300

    Added styles for navigation - in navigation.js(for now)

--- app/scripts/calendar/navigation.js
+++ app/scripts/calendar/navigation.js
@@ -3,10 +3,7 @@ define('navigation', ['jquery', 'underscore', 'handlebars', 'calendar/database',
 		$.fn.fillnavigation = function() {
 			var $this = this;
 			var logger = {
-				logged: false,
-				toMonthView: function(){
-					//TODO
-				}
+				logged: false
 			};
 
 			var sorce = $('#navigation-template').html();
@@ -14,12 +11,71 @@ define('navigation', ['jquery', 'underscore', 'handlebars', 'calendar/database',
 
 			var result = $(template(logger)).appendTo($this);
 
-			//if(logger.logged)
-			//{
-			//	var logout = $this.('#logout').addClass('log');
-			//} else {
-			//	var login = $this.('#login').addClass('log');
-			//}
+			var navContainer = $('#navigation-container')
+			.css({
+				'overflow': 'auto',
+				'zoom': '1',
+				'background-color': '#73AFB6',
+				'color': '#ED1C24',
+				'padding': '5px'
+			});
+			var leftFloated = $('.left')
+			.css({
+				'float': 'left',
+				'padding': '3px'
+			});
+			var rightFloated = $('.right')
+			.css({
+				'float': 'right',
+				'padding': '3px'
+			});
+			var home = $('#home')
+			.addClass('item')
+			.css({
+				'position': 'relative',
+			});
 
+
+			if(logger.logged)
+			{
+				var logout = $('#logout')
+				.addClass('log item')
+				.click(logOut());
+
+				var userCalender = $('#user-calendar')
+				.addClass('item')
+				.click(monthView());
+
+			} else {
+				var login = $('#login')
+				.addClass('log item')
+				.click(logInForm());
+
+				var creatNew  = $('#create-new-account')
+				.addClass('item')
+				.click(createNewAccount());
+			}
+
+			var items = $('.item')
+			.css({
+				'margin': '3px',
+				'cursor': 'pointer'
+			})
+
+			function logOut() {
+				//TODO
+			};
+
+			function monthView(){
+				//TODO
+			};
+
+			function logInForm(){
+				//TODO
+			};
+
+			function createNewAccount(){
+				//TODO
+			};
 		}
 	});
