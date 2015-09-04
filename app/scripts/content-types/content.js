4befb8d6d4d5828d487e424f1798ff8871b83889
Peter Velkov <kidroca@gmail.com>
2015-09-04 11:43:18 +0300

    Added validation for content properties

--- app/scripts/content-types/content.js
+++ app/scripts/content-types/content.js
@@ -1,9 +1,55 @@
 define(['jquery'], function($) {
 	function Content(title, type, time) {
-		this.title = title;
-		this.type = type || null;
-		this.time = time || null;
-	}
+	this.title = title;
+	this.type = type || null;
+	this.time = time || null;
+		
+	Object.defineProperties(this, {
+		type: {
+			get: function() {
+				return this._type;
+			},	
+			set: function(val) {
+				if(typeOf(val) !== 'string' || val !== null ){
+					throw new Error('Contenet type is invalid');
+				}
+				this._type = val;
+			},
+			enumerable: true
+		},
+		
+		title: {
+			get: function() {
+				return this._title;
+			},	
+			set: function(val) {
+				if(!val) {
+					throw new Error('Title cannot be empty or undefined');
+				}
+				
+				this._title = val;
+			},
+			enumerable:true
+		},
+		
+		time: {
+			get: function() {
+				return this._time;
+			},	
+			set: function(val) {
+				if (val !== null) {
+					if(val.length <= 5) {
+						throw new Error('Invalid time format: supported are 16:00 or 16 00 or 16h.');
+						this._time = val;
+					}	
+				} else {
+					this._time = null;
+				}
+			},
+			enumerable: true
+		}
+	});
+}
 
 	Object.defineProperties(Content.prototype, {
 		toDomElement: {
