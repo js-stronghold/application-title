640d4b704595e45ecc863a2d78429c87830b32fe
Peter Velkov <kidroca@gmail.com>
2015-09-04 16:17:26 +0300

    small changes in the way Note is created

--- app/scripts/content-types/note.js
+++ app/scripts/content-types/note.js
@@ -1,28 +1,30 @@
 define(['content-types/content', 'jquery'], function(Content, $) {
-function Note(title, message, time) {
-	var TYPE = 'note';
-		Content.call(this,
-			title.title || title,
-			TYPE,
-			title.time || time);
-			
-	Object.defineProperties(this, {
-		message: {
-			get: function() {
-				return this._message;
-			},
-			set: function(val) {
-				if(!val || typeof(val) !== 'string') {
-					throw new Error('Note received invalid message not a string or empty');
-				}
-				
-				this._message = val;
-			},
-			enumerable: true
-		}
-	});		
+	function Note(title, message, time) {
+		var TYPE = 'note';
 
-	this.message = title.message || message;	
+		Object.defineProperties(this, {
+			message: {
+				get: function() {
+					return this._message;
+				},
+				set: function(val) {
+					if (!val || typeof(val) !== 'string') {
+						throw new Error('Note received invalid message not a string or empty');
+					}
+
+					this._message = val;
+				},
+				enumerable: true
+			}
+		});
+
+		if (title.title) {
+			Content.call(this, title.title, TYPE, title.time);
+			this.message = title.message;
+		} else {
+			Content.call(this, title, TYPE, time);
+			this.message = message;
+		}
 	}
 
 	Note.prototype = Object.create(Content.prototype, {
@@ -45,7 +47,7 @@ function Note(title, message, time) {
 		var noteText = $('<p />')
 			.addClass('note')
 			.append(description)
-			.append(this.message);	
+			.append(this.message);
 
 		var wrapper = Content.prototype.toDomElement.call(this)
 			.append(noteText);
