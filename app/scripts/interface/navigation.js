a5180915339d7a31f2a8acb5fded4a0e4fcbb4f1
Peter Velkov <kidroca@gmail.com>
2015-09-09 00:03:40 +0300

    Redone navigation module

--- app/scripts/interface/navigation.js
+++ app/scripts/interface/navigation.js
@@ -1,81 +1,128 @@
-define('navigation', ['jquery', 'underscore', 'handlebars', 'calendar/database', 'extensions/date'],
-	function($, _, Handlebars, database) {
-		$.fn.fillnavigation = function() {
-			var $this = this;
-			var logger = {
-				logged: false
-			};
+define([
+		'jquery',
+		'underscore',
+		'handlebars',
+		'service-communication/request',
+		'service-communication/user',
+		'sammy',
+		'service-communication/login-out-register',
+		'./notifications',
+		'calendar/calendar',
+		'extensions/date'
+	],
+	function($, _, Handlebars, request, currentUser, Sammy, logIn, notify, calendar) {
 
+		var mainContainer = $('#main-container'),
+			navigationContainer = $('#navigation-container');
+
+		currentUser.initPromise
+			.then(function(res) {
+				console.log('filling navigation');
+				fillnavigation(navigationContainer, currentUser);
+				if (currentUser.isLoggedIn) {
+					window.location.hash = '#/calendar';
+				}
+			})
+			.catch(function(err) {
+				notify.printError(err);
+			});
+
+		var sammyApp = Sammy('#main-container', function() {
+			this.get('#/', function() {
+				this.redirect('#/home');
+			});
+
+			this.get('#/home', function() {
+				var self = this;
+				mainContainer.children().detach();
+				fillnavigation(navigationContainer, currentUser);
+				if (currentUser.isLoggedIn) {
+					self.redirect('#/calendar');
+				}
+			});
+
+			this.get('#/register', function() {
+				if (currentUser.isLoggedIn) {
+					this.redirect('#/home');
+				} else {
+					createNewAccount();
+				}
+			});
+
+			this.get('#/login', function() {
+				if (currentUser.isLoggedIn) {
+					this.redirect('#/home');
+				} else {
+					logInForm();
+				}
+			});
+
+			this.get('#/guest', function() {
+				logIn.guestMode();
+			});
+
+			this.get('#/logout', function() {
+				var self = this;
+				logIn.signOut()
+					.then(function() {
+						self.redirect('#/home');
+					})
+					.catch(function(err) {
+						notify.printError(err);
+					});
+			});
+
+			this.get('#/calendar', function() {
+				if (!currentUser.isLoggedIn) {
+					this.redirect('#/home');
+				}
+				calendar.appendTo(mainContainer);
+			});
+		});
+
+		sammyApp.run('#/');
+
+		function monthView() {
+			//TODO
+		}
+
+		function logInForm() {
+			request.GET('forms', {
+				filter: {
+					name: 'login'
+				},
+				fields: {
+					template: 1,
+					Id: 0
+				}
+			}).then(function(result) {
+				logIn.logIn(result[0].template, mainContainer);
+			}).catch(function(err) {
+				notify.printError(err);
+			});
+		}
+
+		function createNewAccount() {
+			request.GET('forms', {
+				filter: {
+					name: 'registration'
+				},
+				fields: {
+					template: 1,
+					Id: 0
+				}
+			}).then(function(result) {
+				logIn.register(result[0].template, mainContainer);
+			});
+		}
+
+		function fillnavigation(container, params) {
 			var sorce = $('#navigation-template').html();
 			var template = Handlebars.compile(sorce);
+			var result = $(template(params));
 
-			var result = $(template(logger)).appendTo($this);
-
-			var navContainer = $('#navigation-container')
-			.css({
-				'overflow': 'auto',
-				'zoom': '1',
-				'background-color': '#73AFB6',
-				'color': '#ED1C24',
-				'padding': '5px'
-			});
-			var leftFloated = $('.left')
-			.css({
-				'float': 'left',
-				'padding': '3px'
-			});
-			var rightFloated = $('.right')
-			.css({
-				'float': 'right',
-				'padding': '3px'
-			});
-			var home = $('#home')
-			.addClass('item')
-			.css({
-				'position': 'relative',
-			});
-
-
-			if(logger.logged)
-			{
-				var logout = $('#logout')
-				.addClass('log item')
-				.click(logOut());
-
-				var userCalender = $('#user-calendar')
-				.addClass('item')
-				.click(monthView());
-
-			} else {
-				var login = $('#login')
-				.addClass('log item')
-				.click(logInForm());
-
-				var creatNew = $('#create-new-account')
-				.addClass('item')
-				.click(createNewAccount());
-			}
-
-			var items = $('.item')
-			.css({
-				'margin': '3px',
-				'cursor': 'pointer'
-			});
-
-			function logOut() {
-				//TODO
-			}
-
-			function monthView(){
-				//TODO
-			}
-
-			function logInForm(){
-				//TODO
-			}
-
-			function createNewAccount(){
-				//TODO
-			}
-		};
+			container
+				.empty()
+				.append(result);
+		}
 	});
