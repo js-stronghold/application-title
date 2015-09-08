5b1d18796d47dd7275222e13a485af645299f013
Peter Velkov <kidroca@gmail.com>
2015-09-09 01:11:59 +0300

    redefined the user module

--- app/scripts/service-communication/user.js
+++ app/scripts/service-communication/user.js
@@ -1,51 +1,92 @@
-define('user',['libs/everlive/src/everlive.all', 'calendar/database'], function(Everlive, database){
-	var apiKey = 'Ens6YHzXbKZ7ymKx';
-	var el = new Everlive(apiKey);
-	var query = new Everlive.Query();
+define([
+	'./database-link',
+	'./request',
+	'interface/notifications',
+	'calendar/calendar',
+	'jquery'
+], function(DB, request, notify, calendar, $) {
+	var CONSOLE_STYLE_GOOD = 'color: green',
+		CONSOLE_STYLE_HEAD = 'color: blue; font-weight: bold',
+		CONSOLE_STYLE_BAD = 'color: red',
+		CONSOLE_STYLE_FYI = 'color: #AEB2D0; font-style: italic';
 
-	var usersData = el.data('User');
+	var isLoggedIn = false,
+		username = null,
+		user = {},
+		initPromise = init();	
 
+	function updateInformation(newInformation) {
+		isLoggedIn = true;
+		username = newInformation.DisplayName;
+		console.log('%cUser information updated', CONSOLE_STYLE_FYI);
+	}
 
+	function init() {
+		return request.GET('Users/me', {
+				authorisation: 'Bearer ' + localStorage.getItem('access_token')
+			})
+			.then(function(res) {
+				console.log('%cUser Init...', CONSOLE_STYLE_HEAD);
+				return updateInformation(res);
+			})
+			.then(function() {
+				console.log('%cLoading Database', CONSOLE_STYLE_FYI);
+				return DB.downloadFromCloud();
+			})
+			.then(function() {
+				console.log('%cResetting calendar information', CONSOLE_STYLE_FYI);
+				return calendar.resetContent();
+			})
+			.then(function() {
+				console.log('%c...User Init Successful', CONSOLE_STYLE_HEAD);
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+	}
 
-		return {
-			createNewUser: function(username, password){
-				var user = {
-					password: password,
-					name: username,
-					daysWithContent: []
-				};
-
-				usersData.create(user)
-				.then(function(){
-					console.log('User created!');
-				})
-				.done();
-			},
-			addDayContentToUser: function(currentUsername, day){
-				var tobepushed = database.prepareForLocalStorage(day, true);
-
-				var attributes = {
-				    "$push": {
-				        "daysWithContent": tobepushed
-				    }
-				};
-
-				var filter = {
-				    'Id': "03f99f10-526a-11e5-ada2-a931b865de90"
-				};
-
-				usersData.rawUpdate(attributes, filter);
-			},
-			getUserContent: function(username, password){
-				var searchingUser = {
-					'name': username,
-					'password' : password
-				};
-				return usersData.get(searchingUser).done().result;
-			}
-
-
+	function switchLoggedState() {
+		if (!isLoggedIn || !username) {
+			init();
+		} else {
+			console.log('%cUser signed off', CONSOLE_STYLE_FYI);
+			isLoggedIn = false;
+			username = null;
 		}
+	}
 
+	Object.defineProperties(user, {
+		username: {
+			get: function() {
+				return username;
+			},
+			enumerable: true
+		},
 
+		isLoggedIn: {
+			get: function() {
+				return isLoggedIn;
+			},
+			enumerable: true
+		},
+
+		init: {
+			value: init,
+			enumerable: true
+		},
+
+		initPromise: {
+			get: function() {
+				return initPromise;
+			},
+			enumerable: true
+		},
+
+		switchLoggedState: {
+			value: switchLoggedState,
+			enumerable: true
+		}
 	});
+
+	return user;
+});
