fd2633e132c245df37dfcedf86701b853b4afd17
Peter Velkov <kidroca@gmail.com>
2015-09-09 00:04:33 +0300

    updated configuration paths and map

--- app/config.js
+++ app/config.js
@@ -1,36 +1,37 @@
 System.config({
-  baseURL: "scripts",
-  defaultJSExtensions: true,
-  transpiler: "none",
-  babelOptions: {
-    "optional": [
-      "runtime",
-      "optimisation.modules.system"
-    ]
-  },
-  paths: {
-    "jquery": "libs/jquery-2.1.4",
-    "jquery-ui/*": "libs/jquery-ui/*.js",
-    "underscore": "libs/underscore.js",
-    "handlebars": "libs/handlebars.js",
-    "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*"
-  },
+	baseURL: "scripts",
+	defaultJSExtensions: true,
+	transpiler: "none",
+	babelOptions: {
+		"optional": [
+			"runtime",
+			"optimisation.modules.system"
+		]
+	},
+	map: {
+		"babel": "npm:babel-core@5.8.23",
+		"babel-runtime": "npm:babel-runtime@5.8.20",
+		"core-js": "npm:core-js@1.1.3",
+		"github:jspm/nodelibs-process@0.1.1": {
+			"process": "npm:process@0.10.1"
+		},
+		"npm:babel-runtime@5.8.20": {
+			"process": "github:jspm/nodelibs-process@0.1.1"
+		},
+		"npm:core-js@1.1.3": {
+			"fs": "github:jspm/nodelibs-fs@0.1.2",
+			"process": "github:jspm/nodelibs-process@0.1.1",
+			"systemjs-json": "github:systemjs/plugin-json@0.1.0"
+		}
+	},
 
-  map: {
-    "babel": "npm:babel-core@5.8.23",
-    "babel-runtime": "npm:babel-runtime@5.8.20",
-    "core-js": "npm:core-js@1.1.3",
-    "github:jspm/nodelibs-process@0.1.1": {
-      "process": "npm:process@0.10.1"
-    },
-    "npm:babel-runtime@5.8.20": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    },
-    "npm:core-js@1.1.3": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.1",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    }
-  }
+	paths: {
+    "sammy": "../node_modules/sammy/lib/sammy.js",
+    "jquery": "../node_modules/jquery/dist/jquery",
+    "jquery-ui/*": "../node_modules/jquery-ui/*.js",
+		"underscore": "../node_modules/underscore/underscore.js",
+		"handlebars": "../node_modules/handlebars/dist/handlebars.js",
+		"github:*": "../jspm_packages/github/*",
+		"npm:*": "../jspm_packages/npm/*"
+	}
 });
