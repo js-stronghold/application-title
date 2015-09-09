c4ca9789c714d45de76692acd8e0ecac0f8107bc
Peter Velkov <kidroca@gmail.com>
2015-09-09 12:25:42 +0300

    Updated the search module to request the serached data from the backend

--- app/scripts/interface/search.js
+++ app/scripts/interface/search.js
@@ -1,26 +1,40 @@
-define(['service-communication/database-link', 'interface/day-view', 'jquery', 'underscore'], function(DB, dayView, $, _) {
+define([
+	'service-communication/database-link',
+	'service-communication/request',
+	'./day-view',
+	'jquery',
+	'underscore'
+], function(DB, request, dayView, $, _) {
 	var foundObjects = [];
 
-	function start() {
-		var inputBox = $('#search')
+	function apply($searchField) {
+		$searchField
 			.on('keydown', function(evt) {
 				var container,
-					leftPos = 500,
-					topPos = 50;
-				// Enter
-				// console.log(evt.keyCode);
-				if (evt.keyCode === 13) {
-					if (inputBox.val().length > 3) {
-						// evt.preventDefault();
-						container = $('#calendar-container');
-						foundObjects = DB.searchContentsByTitle(inputBox.val());
+					searchData,
+					filterContent;
 
-						// day, $selector, x, y, controlElement, removeCallback
-						_(foundObjects).each(function(day) {
-							dayView.init(day, container, leftPos, topPos, inputBox, removeDayFromDB);
-							leftPos += 25;
-							topPos += 100;
-						});
+				// Enter key
+				if (evt.keyCode === 13) {
+					if ($searchField.val().length > 3) {
+						evt.preventDefault();
+						container = $('#calendar-container');
+
+						searchData = getPattern($searchField.val());
+						filterContent = createFilter(searchData);
+
+						request.GET('Days', {
+								authorisation: 'Bearer ' + localStorage.getItem('access_token'),
+								filter: filterContent
+							})
+							.then(function(res) {
+								console.log(res);
+								displayResult(res, container);
+							})
+							.catch(function(err) {
+								console.log('search error');
+								throw err;
+							});
 					} else {
 						// Throw or nothing
 					}
@@ -28,10 +42,65 @@ define(['service-communication/database-link', 'interface/day-view', 'jquery', '
 			});
 	}
 
+	function getPattern(value) {
+		var separatorIndex,
+			field,
+			data,
+			result = {};
 
-	function removeDayFromDB(day) {
-		DB.removeDay(day);
+		separatorIndex = value.indexOf(':');
+
+		if (separatorIndex > -1) {
+			field = value.substring(0, separatorIndex + 1);
+			result.field = field;
+		}
+
+		data = value.substring(separatorIndex + 1);
+		result.contains = data;
+
+		return result;
 	}
 
-	return start;
+	function createFilter(obj) {
+		var result = {};
+
+		if (obj.field && obj.contains) {
+			result[obj.field] = {
+				'$regex': obj.contains
+			};
+		} else if (obj.contains) {
+			result.contents = {
+				'$elemMatch': {
+					'title': {
+						'$regex': obj.contains
+					}
+				}
+			};
+		} else {
+			// To be...
+		}
+
+		return result;
+	}
+
+	function displayResult(data, container) {
+		var leftPos = 500,
+			topPos = 50,
+			days = DB.parseStorageContent(data);
+			console.log(days);
+
+		
+		_(days).each(function(day) {
+			// day, $selector, x, y, controlElement, removeCallback
+			dayView.init(day, container, leftPos, topPos, container, removeDayFromDB);
+			leftPos += 25;
+			topPos += 100;
+		});
+	}
+
+	function removeDayFromDB(day) {
+		DB.removeDay(day.date);
+	}
+
+	return apply;
 });
